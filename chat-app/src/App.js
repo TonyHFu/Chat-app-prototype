4e6b76b0ec1526e2295640fc843c6dccf23176e8
tofutigerz <hfu3@ualberta.ca>
2022-05-18 22:16:27 -0600

    revert previous

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -90,37 +90,6 @@ function App(props) {
 	});
 
 	useEffect(() => {
-		if (isLoggedIn) {
-			return;
-		}
-		axios
-			.post(
-				`${process.env.REACT_APP_API_ROOT}/login`,
-				{
-					user: {
-						email: `admin1@admin.com`,
-						password: "123456",
-					},
-				},
-				{ withCredentials: true }
-			)
-			.then(response => {
-				if (response.data.logged_in) {
-					setUserState(prev => {
-						return {
-							...prev,
-							isLoggedIn: true,
-							user: response.data.user,
-						};
-					});
-				} else {
-					alert("error logging in");
-				}
-			})
-			.catch(error => console.log("api errors:", error));
-	}, []);
-
-	useEffect(() => {
 		//Skip if not logged in
 		if (!isLoggedIn) {
 			return;
